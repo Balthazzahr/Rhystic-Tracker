82ce821cb0147fba338e3bf02b1ec925e3b8bbee
Dave Pople <davepople@gmail.com>
2026-08-17 14:59:00 +1000

    chore: clean up root directory files and hide internal scripts from public repository

--- launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Launcher for Rhystic Tracker (release build, self-contained frontend).
-cd "$(dirname "$0")"
-export GDK_BACKEND=x11
-export WEBKIT_DISABLE_COMPOSITING_MODE=1
-export RHYSTIC_ENV=production
-exec ./src-tauri/target/release/rhystic-tracker
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export GDK_BACKEND=x11
-export WEBKIT_DISABLE_COMPOSITING_MODE=1
-cd "$(dirname "$0")"
-./src-tauri/target/debug/rhystic-tracker
